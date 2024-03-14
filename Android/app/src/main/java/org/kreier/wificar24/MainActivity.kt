97de51d16ff81d45bfdc853481ef48382f8caf19
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2024-03-15 01:28:41 +0700

    Stacked composition in a column with Jetpack starts working

--- Android/app/src/main/java/org/kreier/wificar24/MainActivity.kt
+++ Android/app/src/main/java/org/kreier/wificar24/MainActivity.kt
@@ -3,6 +3,7 @@ package org.kreier.wificar24
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -33,16 +34,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    SmallTopAppBarExample()
-    Text(
-        text = "Eine neue Zeile.",
-        modifier = modifier.padding(24.dp)
-    )
-    HelloContent()
+    Column(
+//        modifier = Modifier
+//            .statusBarsPadding()
+//            .padding(horizontal = 40.dp
+        ) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+            Text(
+                text = "Eine neue Zeile.",
+                modifier = modifier.padding(24.dp)
+            )
+            HelloContent()
+        }
+
+
+//    SmallTopAppBarExample()
+
 //    TextButton(
 //        content = Button_1(nummer = RowScope()),
 //        onClick = action(5)
