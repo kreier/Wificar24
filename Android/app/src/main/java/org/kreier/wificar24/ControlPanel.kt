e259bacd6f7b581c7458845e15500a79cce2bb56
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2024-03-15 19:54:53 +0700

    Simplified toolbar full width

--- Android/app/src/main/java/org/kreier/wificar24/ControlPanel.kt
+++ Android/app/src/main/java/org/kreier/wificar24/ControlPanel.kt
@@ -1,12 +1,9 @@
 package org.kreier.wificar24
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
--- Android/app/src/main/java/org/kreier/wificar24/ToolBar.kt
+++ Android/app/src/main/java/org/kreier/wificar24/ToolBar.kt
@@ -1,43 +1,78 @@
 package org.kreier.wificar24
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-public fun ToolBar24() {
+fun ToolBar24() {
     Row(modifier = Modifier
-        .padding(16.dp)
-        .background(color = Color.DarkGray)
+//        .padding(horizontal = 8.dp)
+        .fillMaxWidth()
+        .bottomBorder(1.dp, DarkGray)
     ) {
         Icon(
             Icons.Rounded.Menu,
-            contentDescription = stringResource(id = androidx.compose.material3.R.string.suggestions_available)
+            contentDescription = "Menu"
         )
         Text(
             text = "Connected to ESP8266",
-            modifier = Modifier.padding(bottom = 8.dp),
             style = MaterialTheme.typography.bodyMedium
         )
+        Spacer(modifier = Modifier.weight(1f))
         Icon(
-            Icons.Rounded.ArrowBack,
-            contentDescription = stringResource(id = androidx.compose.material3.R.string.suggestions_available)
+            imageVector = Icons.Rounded.Search,
+            contentDescription = "Search"
         )
-        Text(text = "asdf")
+        Icon(
+            imageVector = Icons.Default.Settings,
+            contentDescription = "Settings"
+        )
+//        IconButton(onClick = { Log.d("Click", "IconExample")}) {
+//            Icon(
+//                imageVector = Icons.Default.Settings,
+//                contentDescription = "Settings"
+//            )
+//        }
     }
 }
+
+fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height - strokeWidthPx/2
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = height),
+                end = Offset(x = width , y = height),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
 
 @Preview
 @Composable
